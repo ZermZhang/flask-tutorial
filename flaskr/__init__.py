8a28c1492f4598321764c18ae08a5c7185dc628f
ZermZhang <zhang371312@126.com>
2021-06-09 09:19:46 +0800

    [feat]: 初步优化blog页面

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -10,17 +10,23 @@
 """
 import os
 from flask import Flask
+from datetime import timedelta
+
 from . import db
 from . import auth
 from . import blog
+
+from flask_bootstrap import Bootstrap
 
 
 def create_app(test_config=None):
     # create and config the app
     app = Flask(__name__, instance_relative_config=True)
+    Bootstrap(app)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        SEND_FILE_MAX_AGE_DEFAULT=timedelta(seconds=1)
     )
 
     if test_config is None:
