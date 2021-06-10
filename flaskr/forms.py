6be16f2875f49c5fca5539f53ca85ff9ded774d1
ZermZhang <zhang371312@126.com>
2021-06-10 19:26:20 +0800

    [feat]: 首页增加文章名

---
+++ flaskr/forms.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File           : forms
+@Software       : PyCharm
+@Modify Time    : 2021/6/10 19:25
+@Author         : zermzhang
+@version        : 1.0
+@Desciption     :
+"""
