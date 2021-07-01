8e95f27e00a9ae6d76a6a1f3d6df718c0567c43b
ZermZhang <zhang371312@126.com>
2021-07-01 19:58:01 +0800

    [feat]: 增加新的表单提交逻辑

--- flaskr/libs/forms.py
+++ flaskr/libs/forms.py
@@ -9,7 +9,7 @@
 @Desciption     :
 """
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, SelectMultipleField
 from wtforms.validators import DataRequired, Length
 
 
@@ -37,3 +37,31 @@ class ArticleForm(FlaskForm):
     content = StringField(
         validators=[DataRequired('文章内容不能为空')]
     )
+
+
+class PostForm(FlaskForm):
+    """
+    通过markdown编辑和提交文章
+    """
+    title = StringField(
+        validators=[
+            DataRequired('标题不能为空'),
+            Length(max=60, message='标题不要超过60个字符')
+        ]
+    )
+    content_markdown = TextAreaField()
+    content = TextAreaField()
+    categories = SelectMultipleField(
+        validators=[
+            DataRequired('必须选择一个分类')
+        ],
+        coerce=int
+    )
+    can_comment = BooleanField(label='允许评论')
+    description = TextAreaField(
+        validators=[
+            Length(max=150, message='描述信息不要超过150个字符')
+        ]
+    )
+    publish = SubmitField('发布')
+    save = SubmitField('保存为草稿')
