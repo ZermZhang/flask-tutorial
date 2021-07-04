6e363b524fbc7d780814b954e92622d7f9d92d0a
ZermZhang <zhang371312@126.com>
2021-07-04 21:25:02 +0800

    [feat]: 增加编辑框的markdown支持

--- flaskr/web/admin.py
+++ flaskr/web/admin.py
@@ -13,7 +13,7 @@ from flask import (
 )
 
 from flaskr.web.auth import login_required
-from flaskr.libs.forms import ArticleForm
+from flaskr.libs.forms import ArticleForm, PostForm
 from flaskr.db import get_db
 
 bp = Blueprint('admin', __name__, url_prefix='/admin')
@@ -44,3 +44,14 @@ def admin_create():
     else:
         print("Something wrong!")
     return render_template('admin/admin_create.html', form=form)
+
+
+@bp.route('/admin_mdeditor', methods=['GET', 'POST'])
+@login_required
+def admin_mdeditor():
+    form = PostForm(request.form)
+    print(form.title.data)
+    print(form.content_markdown.data)
+    print(form.description.data)
+    print(form.validate_on_submit())
+    return render_template('admin/admin_mdeditor.html', form=form)
